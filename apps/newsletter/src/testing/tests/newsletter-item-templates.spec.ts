5fb873c19750e7b1205dba5354205ccbbabfcd25
isaacparsons <isaac.2962@gmail.com>
2025-03-13 13:22:21 -0600

    clean up

--- apps/newsletter/src/testing/tests/newsletter-item-templates.spec.ts
+++ apps/newsletter/src/testing/tests/newsletter-item-templates.spec.ts
@@ -19,133 +19,136 @@
 // Date.now = jest.fn(() => now.getTime());
 
 // const newsletterName = 'test newsletter 1';
-// describe('newsletter item template routes', () => {
-//   let newsletter;
-//   // let user;
+describe('newsletter item template routes', () => {
+  test('placeholder', () => {
+    expect(1).toEqual(1);
+  });
+  //   let newsletter;
+  //   // let user;
 
-//   beforeAll(async () => {
-//     // user = await createUser();
-//     newsletter = await createNewsletter(1, newsletterName);
-//   });
+  //   beforeAll(async () => {
+  //     // user = await createUser();
+  //     newsletter = await createNewsletter(1, newsletterName);
+  //   });
 
-//   const movieReviewInput: CreateNewsletterPostTemplateInput = {
-//     name: 'movie review',
-//     data: [
-//       {
-//         temp: {
-//           id: 1,
-//           parentId: null,
-//           nextId: 2,
-//           prevId: null,
-//         },
-//         data: {
-//           type: 'text',
-//           name: 'thoughts',
-//           description: 'it was good',
-//         },
-//       },
-//       {
-//         temp: {
-//           id: 2,
-//           parentId: null,
-//           nextId: null,
-//           prevId: 1,
-//         },
-//         data: {
-//           type: 'text',
-//           name: 'rating',
-//           description: '8/10',
-//         },
-//       },
-//     ],
-//   };
+  //   const movieReviewInput: CreateNewsletterPostTemplateInput = {
+  //     name: 'movie review',
+  //     data: [
+  //       {
+  //         temp: {
+  //           id: 1,
+  //           parentId: null,
+  //           nextId: 2,
+  //           prevId: null,
+  //         },
+  //         data: {
+  //           type: 'text',
+  //           name: 'thoughts',
+  //           description: 'it was good',
+  //         },
+  //       },
+  //       {
+  //         temp: {
+  //           id: 2,
+  //           parentId: null,
+  //           nextId: null,
+  //           prevId: 1,
+  //         },
+  //         data: {
+  //           type: 'text',
+  //           name: 'rating',
+  //           description: '8/10',
+  //         },
+  //       },
+  //     ],
+  //   };
 
-//   describe('create newsletter item template', () => {
-//     test('create a movie review template', async () => {
-//       const templateId = await testCaller.newsletterItemTemplates.create(
-//         movieReviewInput
-//       );
+  //   describe('create newsletter item template', () => {
+  //     test('create a movie review template', async () => {
+  //       const templateId = await testCaller.newsletterItemTemplates.create(
+  //         movieReviewInput
+  //       );
 
-//       const result = await testCaller.newsletterItemTemplates.get({
-//         id: templateId,
-//       });
-//       console.log('template', JSON.stringify(result, null, 6));
-//     });
-//     test('create a movie theatre review template', async () => {
-//       const templateId = await testCaller.newsletterItemTemplates.create(
-//         movieReviewInput
-//       );
+  //       const result = await testCaller.newsletterItemTemplates.get({
+  //         id: templateId,
+  //       });
+  //       console.log('template', JSON.stringify(result, null, 6));
+  //     });
+  //     test('create a movie theatre review template', async () => {
+  //       const templateId = await testCaller.newsletterItemTemplates.create(
+  //         movieReviewInput
+  //       );
 
-//       const movieTheatreReviewInput: CreateNewsletterPostTemplateInput = {
-//         name: 'movie theatre review',
-//         data: [
-//           {
-//             temp: {
-//               id: 1,
-//               parentId: null,
-//               nextId: 4,
-//               prevId: null,
-//             },
-//           },
-//           {
-//             temp: {
-//               id: 2,
-//               parentId: 1,
-//               nextId: 3,
-//               prevId: null,
-//             },
-//             data: {
-//               type: 'text',
-//               name: 'distance',
-//               description: 'it was close',
-//             },
-//           },
-//           {
-//             temp: {
-//               id: 3,
-//               parentId: 1,
-//               nextId: null,
-//               prevId: 2,
-//             },
-//             data: {
-//               type: 'text',
-//               name: 'snacks',
-//               description: 'large selection',
-//             },
-//           },
-//           {
-//             temp: {
-//               id: 4,
-//               parentId: null,
-//               nextId: 5,
-//               prevId: 1,
-//             },
-//             templateId: templateId,
-//           },
-//           {
-//             temp: {
-//               id: 5,
-//               parentId: null,
-//               nextId: null,
-//               prevId: 4,
-//             },
-//             data: {
-//               type: 'text',
-//               name: 'Conclusion',
-//               description: '',
-//             },
-//           },
-//         ],
-//       };
+  //       const movieTheatreReviewInput: CreateNewsletterPostTemplateInput = {
+  //         name: 'movie theatre review',
+  //         data: [
+  //           {
+  //             temp: {
+  //               id: 1,
+  //               parentId: null,
+  //               nextId: 4,
+  //               prevId: null,
+  //             },
+  //           },
+  //           {
+  //             temp: {
+  //               id: 2,
+  //               parentId: 1,
+  //               nextId: 3,
+  //               prevId: null,
+  //             },
+  //             data: {
+  //               type: 'text',
+  //               name: 'distance',
+  //               description: 'it was close',
+  //             },
+  //           },
+  //           {
+  //             temp: {
+  //               id: 3,
+  //               parentId: 1,
+  //               nextId: null,
+  //               prevId: 2,
+  //             },
+  //             data: {
+  //               type: 'text',
+  //               name: 'snacks',
+  //               description: 'large selection',
+  //             },
+  //           },
+  //           {
+  //             temp: {
+  //               id: 4,
+  //               parentId: null,
+  //               nextId: 5,
+  //               prevId: 1,
+  //             },
+  //             templateId: templateId,
+  //           },
+  //           {
+  //             temp: {
+  //               id: 5,
+  //               parentId: null,
+  //               nextId: null,
+  //               prevId: 4,
+  //             },
+  //             data: {
+  //               type: 'text',
+  //               name: 'Conclusion',
+  //               description: '',
+  //             },
+  //           },
+  //         ],
+  //       };
 
-//       const movieTheatreTemplateId = await testCaller.newsletterItemTemplates.create(
-//         movieTheatreReviewInput
-//       );
+  //       const movieTheatreTemplateId = await testCaller.newsletterItemTemplates.create(
+  //         movieTheatreReviewInput
+  //       );
 
-//       const result = await testCaller.newsletterItemTemplates.get({
-//         id: movieTheatreTemplateId,
-//       });
-//       console.log('template', JSON.stringify(result, null, 6));
-//     });
-//   });
-// });
+  //       const result = await testCaller.newsletterItemTemplates.get({
+  //         id: movieTheatreTemplateId,
+  //       });
+  //       console.log('template', JSON.stringify(result, null, 6));
+  //     });
+  //   });
+});
